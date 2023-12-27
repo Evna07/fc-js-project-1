be259d7aec88ad6f6974b9cd5bbe4a86137811ab
dariapgrabowska <dariapgrabowska@gmail.com>
2023-12-27 19:17:38 +0100

    added simplehyml, css and js code to the files

---
+++ index.js
@@ -0,0 +1,7 @@
+let dodajPrzychod = document.querySelector("#dodajPrzychod");
+let dodajWydatek = document.querySelector("#dodajWydatek");
+let nazwaPrzychodu = document.querySelector("przychodNazwaPole");
+
+dodajPrzychod.addEventListener("click", function () {
+  
+})
