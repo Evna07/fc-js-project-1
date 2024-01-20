2e77f469debdc6f4de72ea194ba7b3acb3abcdca
dariapgrabowska <dariapgrabowska@gmail.com>
2024-01-20 19:38:20 +0100

    scrapped and rewrote html and css for clarity and less clutter

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-let dodajPrzychod = document.querySelector("#dodajPrzychod");
-let dodajWydatek = document.querySelector("#dodajWydatek");
-let nazwaPrzychodu = document.querySelector("przychodNazwaPole");
+let addTransaction = document.querySelector("#addTransaction");
+let transactionList = document.querySelector("#transactionList");
+let description = document.querySelector("#description");
+let amount = document.querySelector("#amount");
+let select = document.querySelector("#type");
+let balance = document.querySelector("#balance");
+let total = [];
 
-dodajPrzychod.addEventListener("click", function () {
-  
-})
+const balanceTotal = document.createElement("span");
+balance.appendChild(balanceTotal);
+
+/*PRZYCISK DO DODAWANIA WPISU*/
+addTransaction.addEventListener("click", function () {
+  const li = document.createElement("li");
+  li.innerHTML = `<span>${description.value} </span> <span>${amount.value} </span><span>PLN</span>`;
+  if (description.value !== "" && amount.value !== "") {
+    if (type.value === "income") {
+      li.classList.add("income");
+    } else li.classList.add("expense");
+    transactionList.appendChild(li);
+    //!dodac dokladanie obiektu do tablicy: {id, type(inc/exp), value}
+    /*PRZYCISK DO ZMIENIANIA WARTOSCI*/
+    const buttonEdit = document.createElement("button");
+    buttonEdit.innerHTML = "Edytuj";
+    buttonEdit.addEventListener("click", function () {
+      alert("Podaj nowa wartosc");
+    });
+    li.appendChild(buttonEdit);
+    /*PRZYCISK DO USUWANIA WPISU*/
+    const buttonDel = document.createElement("button");
+    buttonDel.innerHTML = "Usuń";
+    li.appendChild(buttonDel); //!dopracowac
+    buttonDel.addEventListener("click", function () {
+      li.innerHTML = "";
+    });
+  }
+  total.push(Number(amount.value));
+
+  balanceTotal.innerHTML = `Bieżące salo: ${total.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  )}`;
+});
